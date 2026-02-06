2774e8846c30dd464cb0bdeb737fb0fdf1df728b
levi <cgfeel@gmail.com>
2026-02-06 17:17:49 +0800

    fix: add lint

--- packages/webpack-rsc/src/client/client-references.ts
+++ packages/webpack-rsc/src/client/client-references.ts
@@ -1,7 +1,7 @@
 import { registerClientReference } from "react-server-dom-webpack/server";
 
 export const ClientCounter = registerClientReference(
-    function() {
+    () => {
         throw new Error("Counter should not be called on the server");
     },
     "./src/client/ClientCounter.tsx",
